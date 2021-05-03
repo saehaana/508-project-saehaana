a6c8372ca19353e38a43c98603eecde1908875d2
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-03 01:41:55 -0400

    html test change

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <html>
 <head>
-<title>Ausawin Saehaan Valorant Stat Tracker</title>
+<title>Valorant Stat Tracker</title>
 </head>
-<body>Problem Statement
-      Ausawin plays a competitive shooter called Valorant and wishes to hit the highest rank of radiant. To do
+<body><br>Problem Statement
+      <br>Ausawin plays a competitive shooter called Valorant and wishes to hit the highest rank of radiant. To do
       this, he wants to personally keep track of his stats within a database in order to help himself visualize
       certain aspects of his gameplay which may help him improve.
 <ul>
-//<li><a href="employees.php">List all employees</a></li>
-//<li><a href="employee.php">List information for an employee</a></li>
-//<li><a href="addEmployee.php">Add an employee</a></li>
-//<li><a href="editEmployee.php">Edit an employee</a></li>
 </ul>
 </body>
 </html>
