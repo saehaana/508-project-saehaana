43ddfd1fe9d449c800753feb6abf50f3f87462a8
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-03 00:57:31 -0400

    test change

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
-<head>
-<title>Ausawin Saehaan Valorant Stat Tracker</title>
+<head> Ausawin Saehaan Valorant Stat Tracker
+<title></title>
 </head>
 <body>Problem Statement
+<br>
       Ausawin plays a competitive shooter called Valorant and wishes to hit the highest rank of radiant. To do
       this, he wants to personally keep track of his stats within a database in order to help himself visualize
       certain aspects of his gameplay which may help him improve.
