241ced432c2c555c6f2a15c823b6bfd6eb53afa5
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-04 21:50:58 -0400

    testing removed use of $errors

--- process.php
+++ process.php
@@ -6,7 +6,7 @@ session_start();
 //connect to server
 $conn = mysqli_connect('localhost','saehaana','V00797462','project_saehaana');
 
-$errors = array(); //initialize array
+//$errors = array(); //initialize array
 
 //if(!empty($_POST['Login'])){
     //get values from form in login.php
@@ -14,11 +14,11 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
     //checks for empty fields in registration form, if empty add into $errors array
-if(empty($username)){ array_push($errors, "Username is required");}
-if(empty($password)){ array_push($errors, "Password is required");}
+//if(empty($username)){ array_push($errors, "Username is required");}
+//if(empty($password)){ array_push($errors, "Password is required");}
 
     //login the user and redirect to home.php
-if(count($errors) == 0){
+//if(count($errors) == 0){
     $password = md5($password); //encrypt password
     $query = "SELECT * FROM Player WHERE username='$username' AND password='$password'";
     $results = mysqli_query($conn,$query);
@@ -27,7 +27,8 @@ if(count($errors) == 0){
         $_SESSION['success'] = "You are now logged in";
         header('location: home.php');
     }else{
-        array_push($errors, "Wrong username or password");
+        echo "Wrong username or password";
+        header('location: index.php');
     }
 }
 //}
