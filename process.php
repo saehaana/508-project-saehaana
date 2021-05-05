b6a31b476e3439dce8cc104b0556c4971c9c1ab3
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-04 21:34:14 -0400

    add errors array to process.php

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 ini_set("display_errors", 1);
 ERROR_REPORTING(E_ALL);
 session_start();
-
+$errors = array(); //initialize array
 //connect to server
 $conn = mysqli_connect('localhost','saehaana','V00797462','project_saehaana');
 
