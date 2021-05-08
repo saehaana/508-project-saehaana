171321af026dd688ee6e858fbb40e686d80320e5
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-08 04:35:43 -0400

    echo email into addmatch

--- addMatch.php
+++ addMatch.php
@@ -36,6 +36,7 @@ else{
         <h1>Match</h1>
         <p>Please select an option from each drop down box</p>
         <form action="matchData.php" method="POST">
+        <?php echo $_SESSION['email']; ?>
         <label for="Game_Status">Game Status:</label>
         <select id="Game_Status" name="Game_Status">
             <option value="Win">Win</option>
