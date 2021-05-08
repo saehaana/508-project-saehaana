c772089d34599dba4f1ef4dc74cc65391131ff4e
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-08 00:55:12 -0400

    session email check

--- matchData.php
+++ matchData.php
@@ -5,6 +5,12 @@ ERROR_REPORTING(E_ALL);
 session_start();
 //connect to server
 $conn = mysqli_connect('localhost','saehaana','V00797462','project_saehaana');
+if(isset($_SESSION['email']){
+    echo $_SESSION['email'];
+}
+else{
+    echo 'email not set';
+}
 
 //get values from addMatch form
 $Game_Status = $_POST['Game_Status'];
@@ -17,7 +23,7 @@ $Weapon = $_POST['Weapon'];
 
 //submit form to db and redirect to home.php
 $queryInsert = "insert into Match_History (Email,Game_Status,Game_Type,Combat_Score,Agent,Map,Weapon)
-values ('echo $_SESSION['email']','$Game_Status','$Game_Type','$Combat_Score','$Agent','$Map','$Weapon')";
+values (echo $_SESSION['email'],'$Game_Status','$Game_Type','$Combat_Score','$Agent','$Map','$Weapon')";
 mysqli_query($conn,$queryInsert);
 //echo "match added successfully, go back to view the match under match history";
 ?>
