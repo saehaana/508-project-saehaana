8a5f0270fc4ebf117848c4a6f690ccde953e4ab9
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-08 00:25:12 -0400

    fixed naming key error when getting values from addMatch form

--- matchData.php
+++ matchData.php
@@ -7,16 +7,16 @@ session_start();
 $conn = mysqli_connect('localhost','saehaana','V00797462','project_saehaana');
 
 //get values from addMatch form
-$game_status = $_POST['Game_Status'];
-$game_type = $_POST['Game_Type'];
-$combat_score = $_POST['Combat_Score'];
-$agent = $_POST['Agent'];
-$map = $_POST['Map'];
-$weapon = $_POST['Weapon'];
+$Game_Status = $_POST['Game_Status'];
+$Game_Type = $_POST['Game_Type'];
+$Combat_Score = $_POST['Combat_Score'];
+$Agent = $_POST['Agent'];
+$Map = $_POST['Map'];
+$Weapon = $_POST['Weapon'];
 
 //submit form to db and redirect to home.php
 $queryInsert = "insert into Match_History (Game_Status,Game_Mode,Combat_Score,Agent,Map,Weapon)
-values ('$game_status','$game_type','$combat_score','$agent','$map','$weapon')";
+values ('$Game_Status','$Game_Type','$Combat_Score','$Agent','$Map','$Weapon')";
 mysqli_query($conn,$queryInsert);
 echo "match added successfully, go back to view the match under match history";
 ?>
