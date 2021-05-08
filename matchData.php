1f6ea3288f6e202b5a92ad713d260b9ac8fcbb57
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-08 16:48:49 -0400

    fixed entire db, edit add match query,

--- matchData.php
+++ matchData.php
@@ -15,10 +15,26 @@ $Agent = $_POST['Agent'];
 $Map = $_POST['Map'];
 $Weapon = $_POST['Weapon'];
 
+//associate all agents with their agent type
+if($Agent == Astra){$AgentType=$_POST['Controller'];}
+
 
 //submit form to db
-$queryInsert = "insert into Match_History (Email,MatchID,Game_Status,Date)
-values ('$email','$Match_ID','$Game_Status','$date')";
-mysqli_query($conn,$queryInsert);
+$queryInsert1 = "insert into Match_History (Email,Game_Status,Date)
+values ('$email','$Game_Status','$date')";
+mysqli_query($conn,$queryInsert1);
+
+$queryInsert2 = "insert into Game_Type (MatchID,Game_Type) values ('$Match_ID','$Game_Type')";
+mysqli_query($conn,$queryInsert2);
+
+$queryInsert3 = "INSERT INTO Combat_Rating (Email,MatchID,RatingNumber) VALUES ('$email','$Match_ID','$Combat_Score')";
+mysqli_query($conn,$queryInsert3);
+
+$queryInsert4 = "INSERT INTO Map (MatchID,MapName) VALUES ('$Match_ID','$Map')";
+mysqli_query($conn,$queryInsert4);
+
+$queryInsert4 = "INSERT INTO Agent (Email,MatchID,AgentName,AgentType) VALUES ('$email','$Match_ID','$Agent','$AgentType')";
+mysqli_query($conn,$queryInsert4);
+
 echo "match added successfully, go back to view the match under match history";
 ?>
