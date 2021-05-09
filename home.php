0577a90aee549b690f76fd1f56fb920752e650b9
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-09 13:38:00 -0400

    added more echos to home to reveal columns

--- home.php
+++ home.php
@@ -45,15 +45,23 @@ $result = mysqli_query($conn,"select MatchID,Game_Status,Date,MapName,AgentName,
 echo"
 <table>
 <tr>
-<th>MatchID</th>
+<th>Match ID</th>
 <th>Game Status</th>
 <th>Date</th>
+<th>Map</th>
+<th>Agent</th>
+<th>Weapon</th>
+<th>Combat Score</th>
 </tr>";
 while($row = mysqli_fetch_array($result)){
 echo "<tr>";
 echo "<td>" . $row['MatchID'] . "</td>";
 echo "<td>" . $row['Game_Status'] . "</td>";
 echo "<td>" . $row['Date'] . "</td>";
+echo "<td>" . $row['MapName'] . "</td>";
+echo "<td>" . $row['AgentName'] . "</td>";
+echo "<td>" . $row['WeaponName'] . "</td>";
+echo "<td>" . $row['RatingNumber'] . "</td>";
 echo "</tr>";
 }
 echo "</table>";
