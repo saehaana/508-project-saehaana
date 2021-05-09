f4a3fab87c9d4a3989375dae072d9ec0373e5ed1
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-09 14:10:50 -0400

    remove match id field

--- addMatch.php
+++ addMatch.php
@@ -32,10 +32,7 @@ if(!isset($_SESSION['username'])) {
         <p>Please select an option from each drop down box</p>
         <form action="matchDataInsert.php" method="POST">
 
-        <p>
-        <label>Match ID:</label>
-        <input type="text" placeholder="Top-right corner of your scorecard" name="Match_ID">
-        </p>
+
         <p>
         <label>Date:</label>
         <input type="date" placeholder="mm/dd/yyyy" name="date">
