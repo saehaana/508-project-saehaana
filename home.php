c689a21819b7849af35ffb02efd1f941b26fe871
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-10 23:57:57 -0400

    display full match history above search bar filter table;clean up home.php indentation and code, search bar filter works!

--- home.php
+++ home.php
@@ -38,7 +38,38 @@ if(!isset($_SESSION['username'])) {
       border: 1px solid black;
     }
     </style>
+
     <h2>Match History</h2>
+    <?php
+    $email = $_SESSION['email'];
+    $result = mysqli_query($conn,"select MatchID,Game_Status,Game_Type,Date,MapName,AgentName,WeaponName,RatingNumber from Match_History join Game_Type using(MatchID) join Map using(MatchID) join Agent using(MatchID) join Weapon using(MatchID) join Combat_Rating using(MatchID) where Email = '$email'");
+    echo"
+    <table>
+    <tr>
+    <th>Match ID</th>
+    <th>Game Status</th>
+    <th>Game Mode</th>
+    <th>Date</th>
+    <th>Map</th>
+    <th>Agent</th>
+    <th>Weapon</th>
+    <th>Combat Score</th>
+    </tr>";
+    while($row = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>" . $row['MatchID'] . "</td>";
+    echo "<td>" . $row['Game_Status'] . "</td>";
+    echo "<td>" . $row['Game_Type'] . "</td>";
+    echo "<td>" . $row['Date'] . "</td>";
+    echo "<td>" . $row['MapName'] . "</td>";
+    echo "<td>" . $row['AgentName'] . "</td>";
+    echo "<td>" . $row['WeaponName'] . "</td>";
+    echo "<td>" . $row['RatingNumber'] . "</td>";
+    echo "</tr>";
+    }
+    echo "</table>";
+    ?>
+
 <p> Or maybe try some of our filters</p>
 <input id='myInput' onkeyup='searchTable()' type='text'>
     <table id='myTable'>
@@ -58,14 +89,14 @@ $result = mysqli_query($conn,"select MatchID,Game_Status,Game_Type,Date,MapName,
 ?>
 <?php while($row = mysqli_fetch_array($result)):?>
 <tr>
-<td><?php echo $row['MatchID'];?> </td>
-<td><?php echo  $row['Game_Status'];?> </td>
-<td><?php echo   $row['Game_Type'];?> </td>
-<td><?php echo   $row['Date'];?> </td>
-<td><?php echo   $row['MapName'];?> </td>
-<td><?php echo   $row['AgentName'];?> </td>
-<td><?php echo   $row['WeaponName'];?> </td>
-<td><?php echo   $row['RatingNumber'];?></td>
+<td><?php echo $row['MatchID'];?></td>
+<td><?php echo $row['Game_Status'];?></td>
+<td><?php echo $row['Game_Type'];?></td>
+<td><?php echo $row['Date'];?></td>
+<td><?php echo $row['MapName'];?></td>
+<td><?php echo $row['AgentName'];?></td>
+<td><?php echo $row['WeaponName'];?></td>
+<td><?php echo $row['RatingNumber'];?></td>
 </tr>
 <?php endwhile;?>
 </table>
@@ -92,7 +123,5 @@ function searchTable() {
     }
 }
 </script>
-
-
 </body>
 </html>
