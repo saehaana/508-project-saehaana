3b59ddd7d7b965fb9b4c489e8f209b74cf749ec1
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-11 11:27:46 -0400

    remove search table from home

--- home.php
+++ home.php
@@ -70,59 +70,5 @@ if(!isset($_SESSION['username'])) {
     }
     echo "</table>";
     ?>
-
-<p> Or maybe try the search filter to compare your games with the above</p>
-<input id='myInput' onkeyup='searchTable()' type='text'>
-    <table id='myTable'>
-    <tr>
-    <th>Match ID</th>
-    <th>Game Status</th>
-    <th>Game Mode</th>
-    <th>Date</th>
-    <th>Map</th>
-    <th>Agent</th>
-    <th>Weapon</th>
-    <th>Combat Score</th>
-    </tr>
-<?php
-$email = $_SESSION['email'];
-$result = mysqli_query($conn,"select MatchID,Game_Status,Game_Type,Date,MapName,AgentName,WeaponName,RatingNumber from Match_History join Game_Type using(MatchID) join Map using(MatchID) join Agent using(MatchID) join Weapon using(MatchID) join Combat_Rating using(MatchID) where Email = '$email'");
-?>
-<?php while($row = mysqli_fetch_array($result)):?>
-<tr>
-<td><?php echo $row['MatchID'];?></td>
-<td><?php echo $row['Game_Status'];?></td>
-<td><?php echo $row['Game_Type'];?></td>
-<td><?php echo $row['Date'];?></td>
-<td><?php echo $row['MapName'];?></td>
-<td><?php echo $row['AgentName'];?></td>
-<td><?php echo $row['WeaponName'];?></td>
-<td><?php echo $row['RatingNumber'];?></td>
-</tr>
-<?php endwhile;?>
-</table>
-<script>
-function searchTable() {
-    var input, filter, found, table, tr, td, i, j;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td");
-        for (j = 0; j < td.length; j++) {
-            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                found = true;
-            }
-        }
-        if (found) {
-            tr[i].style.display = "";
-            found = false;
-        } else {
-            tr[i].style.display = "none";
-        }
-    }
-}
-</script>
 </body>
 </html>
