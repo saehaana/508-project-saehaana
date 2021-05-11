3bf0b645beeeda885bf57bc86a7d5956aca25243
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-11 11:12:06 -0400

    connect compareMatch.php to db

--- compareMatch.php
+++ compareMatch.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+ini_set("display_errors", 1);
+ERROR_REPORTING(E_ALL);
+$conn = mysqli_connect('localhost','saehaana','V00797462','project_saehaana');
+
+//if user not logged in, redirect to index.php
+if(!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+  	header('location: index.php');
+}
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
