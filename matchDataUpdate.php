9e6d81ab9be10f91c4d0fa29422cf0d7e9de87a8
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-11 14:19:49 -0400

    add if!empty to all update queries

--- matchDataUpdate.php
+++ matchDataUpdate.php
@@ -48,33 +48,42 @@ if(!empty($emailUpdate)){
 $queryUpdate2a = "UPDATE Match_History SET Email = '$emailUpdate' where Email = '$email' AND MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate2a);
 }
+if(!empty($Game_StatusUpdate)){
 $queryUpdate2b = "UPDATE Match_History SET Game_Status = '$Game_StatusUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate2b);
-
+}
+if(!empty($dateUpdate)){
 $queryUpdate2c = "UPDATE Match_History SET Date = '$dateUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate2c);
-
+}
+if(!empty($Game_TypeUpdate)){
 $queryUpdate3 = "UPDATE Game_Type SET Game_Type = '$Game_TypeUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate3);
-
+}
+if(!empty($Combat_ScoreUpdate)){
 $queryUpdate4 = "UPDATE Combat_Rating SET RatingNumber = '$Combat_ScoreUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate4);
-
+}
+if(!empty($MapUpdate)){
 $queryUpdate4 = "UPDATE Map SET MapName = '$MapUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate4);
-
+}
+if(!empty($AgentUpdate)){
 $queryUpdate5a = "UPDATE Agent SET AgentName = '$AgentUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate5a);
-
+}
+if(!empty($AgentTypeUpdate)){
 $queryUpdate5b = "UPDATE Agent SET AgentType = '$AgentTypeUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate5b);
-
+}
+if(!empty($WeaponUpdate)){
 $queryUpdate6a = "UPDATE Weapon SET WeaponName = '$WeaponUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate6a);
-
+}
+if(!empty($WeaponTypeUpdate)){
 $queryUpdate6b = "UPDATE Weapon SET WeaponType = '$WeaponTypeUpdate' where MatchID = '$Match_ID'";
 mysqli_query($conn,$queryUpdate6b);
-
+}
 echo "match added successfully, go back to view the match under match history";
 
 ?>
