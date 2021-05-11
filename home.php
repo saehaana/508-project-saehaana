638edc57920a548b772ced6280d1da5cd97f6546
saehaana <61268356+saehaana@users.noreply.github.com>
2021-05-11 12:22:56 -0400

    remove sort stuff

--- home.php
+++ home.php
@@ -72,24 +72,6 @@ $result = mysqli_query($conn,"select MatchID,Game_Status,Game_Type,Date,MapName,
 </tr>
 <?php endwhile;?>
 </table>
-<script>
-function sortTable(table_id, sortColumn){
-    var tableData = document.getElementById(table_id).getElementsByTagName('tbody').item(0);
-    var rowData = tableData.getElementsByTagName('tr');
-    for(var i = 0; i < rowData.length - 1; i++){
-        for(var j = 0; j < rowData.length - (i + 1); j++){
-            if(Number(rowData.item(j).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, "")) < Number(rowData.item(j+1).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, ""))){
-                tableData.insertBefore(rowData.item(j+1),rowData.item(j));
-            }
-        }
-    }
-}
-</script>
-<?php
-$(function(){
-    // pass the id and the <td> place you want to sort by (td counts from 0)
-    sortTable('myTable', 3);
-});
-?>
+
 </body>
 </html>
